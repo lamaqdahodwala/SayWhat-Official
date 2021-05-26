f8ff2b966e20d4708a9c2984f921f67ab8c59817
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-25 20:28:45 -0400

    created signup view

--- saywhat/accounts/views.py
+++ saywhat/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class SignUpView(CreateView):
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('/')
+    form_class = UserCreationForm
