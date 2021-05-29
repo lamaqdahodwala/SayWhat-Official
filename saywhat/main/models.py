8838b51dd4210dd343fb3e69896719a2ebe218c9
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-29 12:16:35 -0400

    created many to many field

--- saywhat/main/models.py
+++ saywhat/main/models.py
@@ -1,17 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 class Post(models.Model):
     op = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=100)
     body = models.TextField()
-    
+    upvotes = models.ManyToManyField(User, related_name='likes')
     def __str__(self):
         return self.title
-
-
-class PostUpvote(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
